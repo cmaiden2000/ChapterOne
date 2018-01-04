5a634795b14156e78fdd1e010a4f012ab3c2a204
cmaiden2000 <31323118+cmaiden2000@users.noreply.github.com>
2018-01-04 11:51:36 -0800

    Add files via upload

---
+++ ProblemThree.java
@@ -0,0 +1,36 @@
+
+/**
+ * Question 1
+ * The language detecter
+ * Christophe Maiden
+ * 
+ * 
+ */
+ // Question 1:
+ // The Language Detector
+ // By: Christophe Maiden
+ // Detects what language is within the print statement
+ 
+ // Question 2:
+ // The blank lines allow the reader to easily seperate each print statement
+ // with its language
+ 
+ // Question 3:
+ // With one slash, the program says that it is an illegal start of the program
+ // only two slashes can be used as a comment
+ 
+ // Question 4:
+ // No this does not cause any problems
+public class ProblemThree
+{
+    public static void main(String args[]){
+        // English // English
+        System.out.println("one two three four five");
+        
+        // French
+        System.out.println("un deux trois quatre cinq");
+        
+        // Spanish
+        System.out.println("uno dos tres cuatro cinco");
+    }
+}
---
+++ ProblemTwo.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class ProblemTwo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ProblemTwo
+{
+    public static void main(String args[]) {
+        System.out.println("Roses are red");
+        System.out.println("Violets are blue");
+        System.out.println("Sugar is sweet");
+        System.out.println("And so are you!");
+    }
+    
+}
